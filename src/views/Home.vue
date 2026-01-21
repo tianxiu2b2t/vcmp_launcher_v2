1d0792fb267c56b8683d6845077c07783546337f
tianxiu2b2t <atianxiua@163.com>
2026-01-22 00:37:49 +0800

    refactor: 更新 Home 组件中的 greet 调用为 computedAsync，并添加 @vueuse/core 依赖

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computedAsync } from '@vueuse/core';
 import { greet } from '../bridge';
 import { invoke } from '@tauri-apps/api/core';
 
-const hello = computed(async () => {
-    return invoke("greet", { name: "World"});
+const hello = computedAsync(async () => {
+    return await greet("name")
 })
 </script>
 
