4766c835d7c558cf3fd12b6c3a5de06b7d36bfb4
tianxiu2b2t <atianxiua@163.com>
2026-01-22 00:31:50 +0800

    refactor: 优化 Home 组件中的 greet 调用并修复 vite 配置中的忽略路径

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { greet } from '../bridge';
 
+const hello = computed(async () => {
+    return await greet("cnm");
+})
 </script>
 
 <template>
-    <h4>{{ await greet("cnm") }}</h4>
+    <h4>{{ hello }}</h4>
 </template>
