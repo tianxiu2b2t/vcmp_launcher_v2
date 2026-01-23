4f4e8459bfcaf9390420c0e2766cf271170d4f38
atianxiua <atianxiua@163.com>
2026-01-24 01:19:17 +0800

    feat: 更新 Settings 组件，添加输入字段以获取互联网服务器和客户端配置

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,18 +2,66 @@
     123
     456
     <InputEdit
-    placeholder="masterlist"/>
+    placeholder="互联网服务器获取网址"
+    :model-value="masterUrl"
+    :update:model-value="masterUrl"/>
+    <InputEdit
+    placeholder="更新客户端获取网址"
+    :model-value="updateUrl"
+    :update:model-value="updateUrl"/>
+    <InputEdit
+    placeholder="更新客户端密码"
+    :model-value="password"
+    :update:model-value="password"/>
+    <InputEdit
+    placeholder="游戏目录"
+    :model-value="gameDir"
+    :update:model-value="gameDir"/>
+    <InputEdit
+    placeholder="用户名"
+    :model-value="username"
+    :update:model-value="username"/>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { getConfig } from '../bridge';
+import { computed, onBeforeMount, onMounted, ref, watch } from 'vue';
+import { getConfig, setConfig } from '../bridge';
 import InputEdit from './InputEdit.vue';
 import { Config } from '../types';
-const _config = ref<Config>(await getConfig())
-const masterUrl = ref(_config.value.internet.master_url)
-const updateUrl = ref(_config.value.internet.update_url);
-const password = ref(_config.value.internet.password);
-const username = ref(_config.value.game.username);
-const game_dir = ref(_config.value.game.game_dir);
+const _config = ref<Config>()
+const outputConfig = computed<Config>(() => {
+    return {
+        ...(_config.value || {}),
+        internet: {
+            master_url: masterUrl.value || '',
+            update_url: updateUrl.value || '',
+            password: password.value || undefined,
+        },
+        game: {
+            game_dir: gameDir.value,
+            username: username.value,
+        }
+    }
+}) 
+const masterUrl = ref<string>();
+const updateUrl = ref<string>();
+const password = ref<string>();
+const gameDir = ref<string>();
+const username = ref<string>();
+console.log(_config)
+onBeforeMount(async () => {
+    _config.value = await getConfig()
+    console.log(_config.value)
+    masterUrl.value = _config.value.internet.master_url
+    updateUrl.value = _config.value.internet.update_url
+    password.value = _config.value.internet.password
+    gameDir.value = _config.value.game.game_dir
+    username.value = _config.value.game.username
+})
+onMounted(() => {
+    watch(outputConfig, async (newVal) => {
+        console.log(newVal)
+    // await setConfig(newVal)  
+})
+})
 </script>
