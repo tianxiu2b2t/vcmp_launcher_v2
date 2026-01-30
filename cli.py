03a3fe94a9e5ae65e271cd0ebe3a9c3b8c237606
tianxiu2b2t <atianxiua@163.com>
2026-01-31 03:39:13 +0800

    feat: 添加 library_redirector 包；重构 cli 脚本以支持新库构建；移除旧的 library_redirect 包

--- cli.py
+++ cli.py
@@ -10,25 +10,35 @@ parser.add_argument('--release', action='store_true', help='build in release mod
 WINDOWS = os.name == 'nt'
 args = parser.parse_args()
 release = args.release
+target = 'i686-pc-windows-' + ('msvc' if WINDOWS else 'gnu')
 
 def build_vcmp_core():
     # first add i686-pc-windows-msvc / i686-pc-windows-gnu 
-    target = 'i686-pc-windows-' + ('msvc' if WINDOWS else 'gnu')
-    subprocess.run(['rustup', 'target', 'add', target])
     subprocess.run(['cargo', 'build', '--target', target, '--release', '--package', 'vcmp_core'])
 
     # cp target/i686-pc-windows-msvc/release/vcmp_core.exe to build/vcmp_core.exe
-    src = Path('target') / target / 'release' / 'vcmp_core.exe'
-    dst = Path('build') / 'vcmp_core.exe'
+
+    cp_build(Path(target) / 'release' / 'vcmp_core.exe')
+
+def cp_build(src: Path):
+    src = Path('target') / src
+    dst = Path('build') / src.name
     dst.parent.mkdir(parents=True, exist_ok=True)
     with open(src, 'rb') as w, open(dst, 'wb') as r:
         r.write(w.read())
+
+
+def build_library_redirector():
+    subprocess.run(['cargo', 'build', '--target', target, '--release', '--package', 'library_redirector'])
+    # dll
+    cp_build(Path(target) / 'release' / 'library_redirector.dll')
 
 def dev_tauri():
     subprocess.run(['yarn', 'tauri', 'dev'], shell=True)
 
     
 if __name__ == '__main__':
+    subprocess.run(['rustup', 'target', 'add', target])
     build_vcmp_core()
     if args.mode == 'dev':
         dev_tauri()
